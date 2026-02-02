609355e1142b0df1411e6844c5e6f8ae1efee374
Lucas Gusmão <lucasgusmaobaptista@gmail.com>
2026-02-02 20:00:38 -0300

    first commit

---
+++ lumina-api/src/main/java/me/lucasgusmao/lumina_api/LuminaApiApplication.java
@@ -0,0 +1,13 @@
+package me.lucasgusmao.lumina_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuminaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuminaApiApplication.class, args);
+	}
+
+}
