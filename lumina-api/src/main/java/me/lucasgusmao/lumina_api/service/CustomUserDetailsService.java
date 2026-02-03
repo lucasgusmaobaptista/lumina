4e2f12875320e93001c738f2ff948a9c339424ad
Lucas Gusmão <lucasgusmaobaptista@gmail.com>
2026-02-03 19:14:39 -0300

    create  CustomUserDetailsService

---
+++ lumina-api/src/main/java/me/lucasgusmao/lumina_api/service/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package me.lucasgusmao.lumina_api.service;
+
+import lombok.RequiredArgsConstructor;
+import me.lucasgusmao.lumina_api.model.entity.User;
+import me.lucasgusmao.lumina_api.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .authorities("ROLE_" + user.getRole().name())
+                .build();
+    }
+}
