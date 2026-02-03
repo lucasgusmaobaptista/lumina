01de496ae04befe57ad03ef72bed74abfce15a90
Lucas Gusmão <lucasgusmaobaptista@gmail.com>
2026-02-03 18:49:39 -0300

    implement findByUsername method

--- lumina-api/src/main/java/me/lucasgusmao/lumina_api/repository/UserRepository.java
+++ lumina-api/src/main/java/me/lucasgusmao/lumina_api/repository/UserRepository.java
@@ -9,7 +9,9 @@ import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
-    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+
+
 
     Optional<User> findByVerificationCode(String code);
 }
